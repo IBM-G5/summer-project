ab84c100e99377714813a4f93b9f240915bb4e3e
siminastefan <simina.stefan@yahoo.com>
2020-07-28 14:36:56 +0300

    added swagger

--- src/main/java/com/summerproject/project/service/TeacherServiceImpl.java
+++ src/main/java/com/summerproject/project/service/TeacherServiceImpl.java
@@ -1,6 +1,8 @@
 package com.summerproject.project.service;
 
 import com.summerproject.project.repository.TeacherRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,9 +12,11 @@ public class TeacherServiceImpl implements TeacherService{
     @Autowired
     private TeacherRepository teacherRepository;
 
+    static final Logger logger = LoggerFactory.getLogger(TeacherServiceImpl.class);
 
     @Override
     public boolean checkLogin(String email, String password) {
+        logger.info("check login with email = " + email + " password = " + password);
         return teacherRepository.existsTeachersByEmailAndPassword(email, password);
     }
 
