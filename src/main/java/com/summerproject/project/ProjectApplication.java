d2e4713e2c365e9acb71ca75d8ea402d338a3b0b
mirunabalaeti <ioana.balaeti99@e-uvt.ro>
2020-07-28 15:21:16 +0300

    edited swagger

--- src/main/java/com/summerproject/project/ProjectApplication.java
+++ src/main/java/com/summerproject/project/ProjectApplication.java
@@ -1,13 +1,29 @@
 package com.summerproject.project;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
+
 public class ProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectApplication.class, args);
 	}
+	@Bean
+	public Docket productApi() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("com.summerproject.project")).build();
+	}
+
 
 }
