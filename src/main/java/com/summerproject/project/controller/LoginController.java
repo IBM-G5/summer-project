e182d52abeb5f5cc8fc994caa49e208e571235e2
siminastefan <simina.stefan@yahoo.com>
2020-07-28 15:30:17 +0300

    swagger final

--- src/main/java/com/summerproject/project/controller/LoginController.java
+++ src/main/java/com/summerproject/project/controller/LoginController.java
@@ -10,25 +10,9 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class LoginController {
+
     @Autowired
     private TeacherService teacherService;
-
-    //Sign Up
-//    @PostMapping
-//    public Teacher addTeacher(@RequestBody Teacher teacher){
-//        return teacherService.addTeacher(teacher);
-//    }
-
-    //SignUp
-    //@PostMapping
-
-
-    //LogIn
-//    @GetMapping(value = "/{teacherName}")
-//    public Teacher findByName(@PathVariable("teacherName") String teacherName){
-//        return teacherService.findByName(teacherName);
-//    }
-
 
     @RequestMapping(method = RequestMethod.POST, value = "/signup")
     public void addTeacher(@RequestBody Teacher teacher){
@@ -37,15 +21,11 @@ public class LoginController {
 
     @RequestMapping(method = RequestMethod.POST, value = "/login")
     public ResponseEntity<String> findTeacherForLogin(@RequestBody Teacher teacher){
-        //return teacherService.findTeacher(teacher.email, info.password);
         boolean result;
         result= teacherService.checkLogin(teacher.email, teacher.password);
-        if (result == true)
-        {
+        if (result) {
             return new ResponseEntity<String>("Autentification succesfull",HttpStatus.OK);
         }
         else  return new ResponseEntity<String>("Autentification failed",HttpStatus.BAD_REQUEST);
-
     }
-
 }
