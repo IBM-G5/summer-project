e4bd13f42df1cc649593eddb8ed44df45be71465
Asofroniei Roxana <asofroniei.roxi@gmail.com>
2020-07-29 01:24:26 +0300

    Update Login

--- frontend/src/components/LoginForm.js
+++ frontend/src/components/LoginForm.js
@@ -9,31 +9,90 @@ import Col from "react-bootstrap/Col";
 import loginSmallSvg from "../img/undraw_safe_bnk7.svg";
 import logoTransparent from "../img/logo_transparent.png";
 
+import axios from "axios";
+
 class LoginForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: "",
+      successMessage: null,
+    };
+
+    this.API_BASE_URL = "localhost:8080/login";
+  }
+
+  redirectToHome(props) {
+    props.updateTitle("Home");
+    props.history.push("/home");
+  }
+
+  handleSubmitClick(e, props) {
+    e.preventDefault();
+    const payload = {
+      email: this.state.email,
+      password: this.state.password,
+    };
+    axios
+      .post(this.API_BASE_URL, payload)
+      .then(function (response) {
+        if (response.data.code === 200) {
+          this.setState((prevState) => ({
+            ...prevState,
+            successMessage: "Login successful. Redirecting to home page..",
+          }));
+          this.redirectToHome();
+          props.showError(null);
+        } else if (response.data.code === 204) {
+          props.showError("Username and password do not match");
+        } else {
+          props.showError("Username does not exists");
+        }
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
   render() {
     return (
       <>
         <p className="text-left">
           <img src={logoTransparent} width="30%" alt="login illustration" />
-          <b>Professors Login</b>
+          Login
         </p>
         <Container className="mt-5">
           <img src={loginSmallSvg} width="55%" alt="login illustration"></img>
-
           <Form className="mt-5">
             <Form.Group controlId="formBasicEmail">
-              <Form.Control type="email" placeholder="Email Address" />
+              <Form.Control
+                type="email"
+                placeholder="Email Address"
+                onChange={(event, newValue) =>
+                  this.setState({ email: newValue })
+                }
+              />
             </Form.Group>
 
             <Form.Group controlId="formBasicPassword">
-              <Form.Control type="password" placeholder="Password" />
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                onChange={(event, newValue) =>
+                  this.setState({ password: newValue })
+                }
+              />
             </Form.Group>
-          </Form>
 
             <Container>
               <Row>
                 <Col>
-                  <Button variant="primary" type="submit">
+                  <Button
+                    variant="primary"
+                    type="submit"
+                    onClick={(event) => this.handleSubmitClick(event)}
+                  >
                     Login
                   </Button>
                 </Col>
@@ -44,14 +103,12 @@ class LoginForm extends Component {
                 </Col>
               </Row>
             </Container>
-          
-
+          </Form>
           <Container className="mt-5 pb-3">
-            <a href="./guest" className="text-muted nounderline small">
-              Not a professor? <b>Continue as a guest</b>
+            <a href="#" className="text-muted nounderline small">
+              Don't have an account? <b>Register</b>
             </a>
           </Container>
-          
         </Container>
       </>
     );
