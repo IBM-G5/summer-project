e6aed7aaafa03855323a063d501dfae13b9e6280
mirunabalaeti <ioana.balaeti99@e-uvt.ro>
2020-08-06 18:21:57 +0300

    created tests

---
+++ src/test/java/com/summerproject/project/controller/ExamScheduleControllerTest.java
@@ -0,0 +1,46 @@
+package com.summerproject.project.controller;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ExamScheduleControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void createExamSchedule() {
+    }
+
+    @Test
+    void removeExamSchedule() {
+    }
+
+    @Test
+    void getAllExamSchedules() {
+    }
+
+    @Test
+    void updateExamSchedule() {
+    }
+
+    @Test
+    void filterByFaculty() {
+    }
+
+    @Test
+    void filterByYearOfStudy() {
+    }
+
+    @Test
+    void filterByYearOfStudyAndFaculty() {
+    }
+}
---
+++ src/test/java/com/summerproject/project/service/ExamScheduleServiceTest.java
@@ -0,0 +1,45 @@
+package com.summerproject.project.service;
+
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ExamScheduleServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void addExamSchedule() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void deleteExamSchedule() {
+    }
+
+    @Test
+    void updateExamSchedule() {
+    }
+
+    @Test
+    void getAllExamSchedules() {
+    }
+
+    @Test
+    void getAllExamSchedulesFilterByFaculty() {
+    }
+
+    @Test
+    void getAllExamSchedulesFilterByYearOfStudy() {
+    }
+
+    @Test
+    void getAllExamSchedulesFilterByYearOfStudyAndFaculty() {
+    }
+}
