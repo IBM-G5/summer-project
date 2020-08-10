5faf8c14edadbe70fc025cc144db4cacac9e375a
DenisiaBianca <paraialadenisia@gmail.com>
2020-08-10 22:46:12 +0300

    update add Exam schedule

--- frontend/src/components/ExamForm.js
+++ frontend/src/components/ExamForm.js
@@ -6,78 +6,114 @@ class ExamForm extends Component {
     constructor(props) {
         super(props);
         this.state={
-            id: '',
-            e_id:'',
-            academicYear: '',
-            f_id: '',
-            f_name: '',
-            c_id: '',
-            c_name: '',
-            yearOfStudy: '',
-            semester: '',
-            t_id: '',
-            t_name: '',
-            email: '',
-            password: '',
-            numberOfSeats: '',
-            date: '',
-            classroom: ''
+            id_state: null,
+            e_id_state: null,
+            academicYear_state: null,
+            f_id_state: null,
+            f_name_state: null,
+            c_id_state: null,
+            c_name_state: null,
+            yearOfStudy_state: null,
+            semester_state: 2,
+            t_id_state: null,
+            t_name_state: null,
+            email_state: "@mail",
+            password_state: "pass",
+            numberOfSeats_state: null,
+            date_state: null,
+            classroom_state: null
         }
     };
 
-    saveHandler=(exam)=>{
-        alert(this.state.f_name +""+ this.state.yearOfStudy);
-        exam.preventDefault();
+    saveHandler=()=>{
+
+        fetch("http://localhost:8080/create",{
+            method: 'POST',
+            body:JSON.stringify({
+                id: `${this.state.id_state}`,
+                exam:{
+                    id: `${this.state.e_id_state}`,
+                    academicYear: `${this.state.academicYear_state}`,
+                    faculty:{
+                        id: `${this.state.f_id_state}`,
+                        name: `${this.state.f_name_state}`
+                    },
+                    course:{
+                        id:`${this.state.c_id_state}`,
+                        name: `${this.state.c_name_state}`,
+                    },
+                    teacher:{
+                        id:`${this.state.t_id_state}`,
+                        name:`${this.state.t_name_state}`,
+                        email:`${this.state.email_state}`,
+                        password:`${this.state.password_state}`
+                    },
+                },
+                numberOfSeats:`${this.state.numberOfSeats_state}`,
+                date: `${this.state.date_state}`,
+                classroom: `${this.state.classroom_state}`
+            })
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log('Success:', data);
+                alert('The exam schedule was saved!')
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
     }
     handlerFacultyChange=(item)=>{
         this.setState({
-            f_name:item.target.value,
+            f_name_state:item.target.value,
         })
     }
     handlerYearOfStudyChange=(item)=>{
         this.setState({
-            yearOfStudy:item.target.value,
+            yearOfStudy_state:item.target.value,
         })
     }
     handlerCourseChange=(item)=>{
         this.setState({
-            c_name:item.target.value,
+            c_name_state:item.target.value,
         })
     }
     handlerAcademicYearChange=(item)=>{
         this.setState({
-            academicYear:item.target.value,
+            academicYear_state:item.target.value,
         })
     }
     handlerTeacherChange=(item)=>{
         this.setState({
-            t_name:item.target.value,
+            t_name_state:item.target.value,
         })
     }
     handlerNoOfSeatsChange=(item)=>{
         this.setState({
-            numberOfSeats:item.target.value,
+            numberOfSeats_state:item.target.value,
         })
     }
     handlerClassRoomChange=(item)=>{
         this.setState({
-            classroom:item.target.value,
+            classroom_state:item.target.value,
         })
     }
     handlerDataChange=(item)=>{
         this.setState({
-            date:item.target.value,
+            date_state:item.target.value,
         })
     }
 
 
     render() {
 
-        const faculty=[ this.state.f_id, this.state.f_name];
-        const course=[this.state.c_id, this.state.c_name, this.state.yearOfStudy, this.state.semester];
-        const teacher=[this.state.t_id, this.state.t_name, this.state.email, this.state.password];
-        const exam=[this.state.e_id, this.state.academicYear, faculty,course,teacher];
-        const examSchedule=[this.state.id, exam, this.state.numberOfSeats, this.state.date, this.state.classroom];
+        // const faculty=[ this.state.f_id, this.state.f_name];
+        // const course=[this.state.c_id, this.state.c_name, this.state.yearOfStudy, this.state.semester];
+        // const teacher=[this.state.t_id, this.state.t_name, this.state.email, this.state.password];
+        // const exam=[this.state.e_id, this.state.academicYear, faculty,course,teacher];
+        // const examSchedule=[this.state.id, exam, this.state.numberOfSeats, this.state.date, this.state.classroom];
+        //
+        // this.state.examToAdd=examSchedule;
 
         const allFaculty = ["Automatica si Calculatoare","Mecanica","Constructii","Arhitectura si Urbanism"];
         const courses = ["Programare", "Algebra", "Analiza Matematica"];
@@ -90,7 +126,8 @@ class ExamForm extends Component {
                     <div className="form-row">
                         <div className="col">
                             <label> Faculty</label>
-                            <select className="form-control" value={this.state.f_name} onChange={this.handlerFacultyChange}>
+                            <select className="form-control" value={this.state.f_name_state} onChange={this.handlerFacultyChange} required>
+                                <option value="" disabled selected>Select a faculty</option>
                                 {
                                     allFaculty.map((f) =>
                                         <option value={f}>{f}</option>
@@ -100,7 +137,8 @@ class ExamForm extends Component {
                         </div>
                         <div className="col">
                             <label>Year of study</label>
-                            <select className="form-control" value={this.state.yearOfStudy} onChange={this.handlerYearOfStudyChange}>
+                            <select className="form-control" value={this.state.yearOfStudy_state} onChange={this.handlerYearOfStudyChange} required>
+                                <option value="" disabled selected>Select the year of study</option>
                                 {
                                     [1, 2, 3, 4, 5, 6].map((y) =>
                                         <option value={y}>{y} year</option>
@@ -113,7 +151,8 @@ class ExamForm extends Component {
                     <div className="form-row">
                         <div className="col">
                             <label>Course</label>
-                            <select className="form-control" value={this.state.c_name} onChange={this.handlerCourseChange}>
+                            <select className="form-control" value={this.state.c_name_state} onChange={this.handlerCourseChange}required>
+                                <option value="" disabled selected>Select a course</option>
                                 {
                                     courses.map((c) =>
                                         <option value={c}>{c}</option>
@@ -123,7 +162,8 @@ class ExamForm extends Component {
                         </div>
                         <div className="col">
                             <label>Teacher</label>
-                            <select className="form-control" value={this.state.t_name} onChange={this.handlerTeacherChange}>
+                            <select className="form-control" value={this.state.t_name_state} onChange={this.handlerTeacherChange}required>
+                                <option value="" disabled selected>Select a teacher</option>
                                 {
                                     teachers.map((t) =>
                                         <option value={t}>{t}</option>
@@ -136,7 +176,8 @@ class ExamForm extends Component {
                     <div className="form-row">
                         <div className="col">
                             <label>Academic Year</label>
-                            <select className="form-control" value={this.state.academicYear} onChange={this.handlerAcademicYearChange}>
+                            <select className="form-control" value={this.state.academicYear_state} onChange={this.handlerAcademicYearChange}required>
+                                <option value="" disabled selected>Select the academic year</option>
                                 {
                                     ["2017","2018","2019","2020"].map((a) =>
                                         <option value={a}>{a}</option>
@@ -146,7 +187,8 @@ class ExamForm extends Component {
                         </div>
                         <div className="col">
                             <label>Number of seats</label>
-                            <select className="form-control" value={this.state.numberOfSeats} onChange={this.handlerNoOfSeatsChange}>
+                            <select className="form-control" value={this.state.numberOfSeats_state} onChange={this.handlerNoOfSeatsChange}required>
+                                <option value="" disabled selected>Select the number of seats</option>
                                 {
                                     [300, 200, 250].map((s) =>
                                         <option value={s}>{s}</option>
@@ -156,7 +198,8 @@ class ExamForm extends Component {
                         </div>
                         <div className="col">
                             <label>Classroom</label>
-                            <select className="form-control" value={this.state.classroom} onChange={this.handlerClassRoomChange}>
+                            <select className="form-control" value={this.state.classroom_state} onChange={this.handlerClassRoomChange}required>
+                                <option value="" disabled selected>Select a classroom</option>
                                 {
                                     rooms.map((r) =>
                                         <option value={r}>{r}</option>
@@ -169,7 +212,7 @@ class ExamForm extends Component {
                     <div className="form-row">
                         <div className="col">
                             <label>Date</label>{'  '}
-                            <input type="date" value={this.state.date} onChange={this.handlerDataChange}></input>
+                            <input type="date" value={this.state.date_state} onChange={this.handlerDataChange} required></input>
                         </div>
                         <div className="col">
                             <label>Time</label>{'  '}
